d678ec6f0eb6a306052c7fd675c48a122291d479
michiel <boeckaert@gmail.com>
2014-09-07 23:37:41 +0200

    removed wrong phpdoc

--- src/Domain/Read/Repository/DoctrineORMRepositoryFactory.php
+++ src/Domain/Read/Repository/DoctrineORMRepositoryFactory.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
-* This file is part of the MilioooFriendsBundle package.
-*
-* (c) Michiel boeckaert <boeckaert@gmail.com>
-* This source file is subject to the MIT license that is bundled
-* with this source code in the file LICENSE.
-*/
-
 namespace Milio\User\Domain\Read\Repository;
 
 use Broadway\ReadModel\RepositoryFactoryInterface;
